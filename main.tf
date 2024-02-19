d380c259ce07f3f10aec61c5b6de0d39a9567925
SINGH SNEH <25021804@mahindra.com>
2024-02-19 18:28:45 +0530

    added cluster redis

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 resource "aws_elasticache_subnet_group" "example" {
-  name       = var.subnet_group_name
+  name       = var.test_subnet_name
   subnet_ids = var.subnet_ids
 }
-resource "aws_elasticache_cluster" "example" {
-  cluster_id = var.name
-  engine                = var.engine
-  engine_version        =var.redis_version
-  network_type          ="ipv4"
+resource "aws_elasticache_replication_group" "example" {
+  replication_group_id = var.name
+  description          = "example with authentication"
   node_type            = var.node_type
-  num_cache_nodes      = 1
-  parameter_group_name = var.parameter_group_name
+  engine               = var.engine
+  num_cache_clusters   =var.num_cache_clusters
   port                 =var.port
   subnet_group_name    = aws_elasticache_subnet_group.example.name
-  availability_zone =var.availability_zone
-  security_group_ids=var.vpc_security_group_ids
-  snapshot_retention_limit=7
+  security_group_ids   =var.security_group_ids
+  parameter_group_name = var.parameter_group_name
+  engine_version       =var.engine_version
+  transit_encryption_enabled =var.transit_encryption_enabled
+  snapshot_retention_limit=var.snapshot_retention_limit
   auto_minor_version_upgrade=true
   apply_immediately = true
 }
--- variables.tf
+++ variables.tf
@@ -1,55 +1,57 @@
 variable "name" {
     type=string
-    description = "name of redis cluster "  
-}
-variable "engine" {
-    type=string
-    description = "engine of redis"
-    default = "redis" 
-}
-variable "redis_version" {
-    type = string
-    description = "version of redis" 
+    description = "name of cluster " 
 }
 variable "node_type" {
     type = string
-    description = "configation of redis" 
+    description = "node type of redis" 
+}
+variable "engine" {
+    type=string
+    description = "engine of instance " 
+}
+variable "engine_version" {
+    type = string
+    description = "engine version of redis" 
+}
+variable "num_cache_clusters" {
+    type=number
+    description = "number of cluster "
+}
+variable "security_group_ids" {
+    type=list(string)
+    description = "Security group id"
 }
 variable "parameter_group_name" {
+    type = string
+    description = "parameter group name"
+}
+variable "test_subnet_name" {
     type=string
-    description = "parameter group name" 
-}
-variable "port" {
-    type=number
-    description = "port for connaction"
-  
-}
-variable "availability_zone" {
-    type=string
-    description = "zone of redis"
-}
-variable "vpc_security_group_ids" {
-    type = list(string)
-    description = "security group id " 
-}
-variable "snapshot_retention_limit" {
-    type=string
-    description = "snapshot retention limit"
-    default = "7"  
+    description = "subnet name for redis" 
 }
 variable "subnet_ids" {
     type=list(string)
-    description = "subnet id for redis"
+    description = "subnet_ids" 
 }
-variable "subnet_group_name" {
-    type=string
-    description = "subnet group name for redis" 
+variable "port" {
+    type=number
+    description = "port for redis"
+    default =  6379
 }
-variable "role_arn" {
-    type = string
-    description = "role arn of account" 
+variable "snapshot_retention_limit" {
+    type=number
+    description = "keep snapshot for backup "
+}
+variable "transit_encryption_enabled" {
+    type=bool
+    description = "transit encryption enabled"
 }
 variable "region" {
-    type = string
-    description = "region " 
+    type=string
+    description = "region of instance"
+}
+variable "role_arn" {
+    type=string
+    description = "role arn"
 }
